32a930cd334097d340ef314dd0b1ede1e6fac920
ohuton55 <tttt.tttt.no@gmail.com>
2023-11-13 19:03:41 +0900

    create admin super user

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
@@ -6,6 +9,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     # 発行日　人間可読フィールド
     pub_date = models.DateTimeField('date published')
+    
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     # Question modelと関連付け
@@ -14,3 +24,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     # 投票　数値フィールド
     votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
