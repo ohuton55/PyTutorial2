0d01b6b776b24368bc5a5bc4280c3aba096a2b2e
ohuton55 <tttt.tttt.no@gmail.com>
2023-11-14 00:55:47 +0900

    Slimmed the views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import get_object_or_404, render
-from django.http  import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
-from django.utils import timezone
+from django.http  import HttpResponseRedirect
+# from django.http  import HttpResponse, Http404
+# from django.template import loader
+# from django.utils import timezone
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
-# Create your views here.
-def index(request):
+#------------------------------------------------------------------
+#def index(request):
     # timezone テスト
     # now = timezone.now()
     # return HttpResponse(timezone.localtime(now))
 
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     
     # ページのデザインと癒着する書き方
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
     
     # latest_question_list ... model Question object 最新５件
-    context = {
-        'latest_question_list': latest_question_list,
-    }
+#    context = {
+#        'latest_question_list': latest_question_list,
+#    }
 
     # ショートカットを使わない書き方
     # get_template -> ./templates/polls/index.html
@@ -31,10 +33,18 @@ def index(request):
     # return HttpResponse(template.render(context, request))
 
     # shortcuts.renderを使う書き方
-    return render(request, 'polls/index.html', context)
+ #   return render(request, 'polls/index.html', context)
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+#------------------------------------------------------------------
+#def detail(request, question_id):
     # ショートカットを使わない書き方
     # try:
     #    question = Question.objects.get(pk=question_id)
@@ -43,19 +53,29 @@ def detail(request, question_id):
     #    raise Http404("Question does not exist. question_id is %s" % question_id)
     
     # shortcuts.get_object_or_404を使う書き方
-    question = get_object_or_404(Question, pk=question_id)
+#    question = get_object_or_404(Question, pk=question_id)
 
-    return render(request, 'polls/detail.html', {'question': question})
+#    return render(request, 'polls/detail.html', {'question': question})
 
     # shortcuts.render()を使わない例
     # %s <= % object
     # return HttpResponse("You're looking at question %s." % question_id)
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+# "pk" という名前で URL からプライマリキーをキャプチャ -> path('<int:pk>/', , )
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+#------------------------------------------------------------------
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question': question})
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+#------------------------------------------------------------------
 
 def vote(request, question_id):
     # question_id ... detail.html formからPOSTされたchoice=#が来る
