556dab04ee7e55505fb3ba1c3c686ee1191fe276
ohuton55 <tttt.tttt.no@gmail.com>
2023-11-14 12:54:26 +0900

    fix typing miss

--- mysite13/polls/views.py
+++ mysite13/polls/views.py
@@ -82,7 +82,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice,DoesNotExist):
+    except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
