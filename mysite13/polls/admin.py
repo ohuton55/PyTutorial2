06a69e9edab1da80d03b9fc917ee109a8f65c343
ohuton55 <tttt.tttt.no@gmail.com>
2023-11-17 11:42:39 +0900

    add link to detail page and form

--- mysite13/polls/admin.py
+++ mysite13/polls/admin.py
@@ -5,6 +5,11 @@ from django.utils.html import format_html
 
 from .models import Question, Choice
 
+# custom action
+@admin.action(description="titleをotherに変更")
+def change_question_title(modeladmin, request, queryset):
+    queryset.update(question_text='other')
+
 # viewとして呼び出す
 class QuestionDetailView(DetailView):
     template_name = "admin/polls/question/new_page.html"
@@ -25,20 +30,24 @@ class QuestionAdmin(admin.ModelAdmin):
             #classes = ["collapse"] 境界を重ねて並べる
     inlines = [ChoiceInline]
     list_display = ["question_text", "pub_date", "was_published_recently","question_new_page"]
+    list_display_links = ["pub_date"]
     list_filter = ["pub_date"]
+    list_editable = ["question_text"]
     search_fields = ["question_text"]
+    actions = [change_question_title]
 
     def get_urls(self):
         urls = super().get_urls()   # defaultのadmin urlをロード
 
-        detail_urls = [
-            path("new_page/<pk>", self.admin_site.admin_view(QuestionDetailView.as_view()), name=f"new_question_page",)
+        new_page_urls = [
+            path("new_page/<pk>", self.admin_site.admin_view(QuestionDetailView.as_view()), name=f"new_question_page"),
         ]
-        return detail_urls + urls
+        return new_page_urls + urls
 
     def question_new_page(self, obj):
         url = reverse("admin:new_question_page", args=[obj.pk])
         return format_html(f'<a href="{url}">&#x1f4dd</a>')
+        
     
 admin.site.register(Question, QuestionAdmin)
 
